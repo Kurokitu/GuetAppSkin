c284d3b0b8308eb82d900f1374d75f3734d0610b
Rubicon Luo <l1589002388@gmail.com>
2019-11-21 18:38:31 +0800

    GUETClient

--- src/plugins/guetsdk/client.js
+++ src/plugins/guetsdk/client.js
@@ -1,1 +1,50 @@
 import BCLI from 'base';
+import { LoginCall } from 'structures';
+
+/* A Client to manage state of API
+Example:
+````
+let client = new GUETClient();
+await client.login('example','example');
+let userInfoResult = await client.send(new UserInfoCall());
+// GUETClient can automatically add 'cookie' field in method send() after login
+````
+*/
+class GUETClient {
+    constructor(){
+        this.userCookie = null;
+    }
+
+    login(username, password){
+        return this.send(new LoginCall(username, password)).then((result) => {
+            this.setUserCookie(result.getUserCookie());
+            return result;
+        });
+    }
+
+    setUserCookie(cookie){
+        this.userCookie = cookie;
+    }
+
+    rawSend(requestConfig){
+        return BCLI(requestConfig);
+    }
+
+    send(call){
+        if (this.userCookie != null){
+            call.setUserCookie(this.userCookie);
+        }
+        this.rawSend(call.makeAxiosRequestConfig()).then((response) => call.getPostprocessor()(response));
+    }
+
+    static withLogin(username, password){
+        return new Promise((resolve, reject) => {
+            let cli = new GUETClient();
+            cli.login(username, password).then(() => resolve(cli), (reason) => reject(reason));
+        });
+    }
+}
+
+export default {
+    GUETClient
+};

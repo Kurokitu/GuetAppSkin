68f93969f707c7a6107985eb1dbf7f7c6667e88e
Rubicon Luo <l1589002388@gmail.com>
2019-11-26 18:35:56 +0800

    change config for tests

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
-module.exports = {
-  presets: [
-    '@vue/app'
-  ]
-}
+module.exports = api => {
+  let config = {
+    presets: [],
+    plugins: [
+      "@babel/plugin-proposal-nullish-coalescing-operator"
+    ]
+  };
+  if (api.env('test')){
+    config.plugins.push("@babel/plugin-transform-modules-commonjs");
+  } else {
+    config.presets.push('@vue/app');
+  }
+  return config;
+};

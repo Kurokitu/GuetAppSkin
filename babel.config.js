3c64225a64625f8a2d259ebe69a9d59bf8bd3f6a
Rubicon Luo <l1589002388@gmail.com>
2019-11-27 18:26:19 +0800

    update babel config

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,21 @@ module.exports = api => {
   let config = {
     presets: [],
     plugins: [
-      "@babel/plugin-proposal-nullish-coalescing-operator"
+      "@babel/plugin-proposal-nullish-coalescing-operator",
+      ["@babel/plugin-proposal-decorators",{
+        decoratorsBeforeExport: true
+      }],
+      "@babel/plugin-proposal-function-bind",
+      "@babel/plugin-proposal-partial-application"
     ]
   };
   if (api.env('test')){
+    config.presets.push(["@babel/preset-env",{
+      targets: {
+        node: "current"
+      },
+      modules: false
+    }]);
     config.plugins.push("@babel/plugin-transform-modules-commonjs");
   } else {
     config.presets.push('@vue/app');

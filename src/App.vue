a48cbb1384a9957ebf81f2edffb72dd8f3af5889
Kurokitu <foxiti.a@gmail.com>
2019-11-28 19:38:08 +0800

    Modify Layout

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,106 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" :clipped="$vuetify.breakpoint.lgAndUp" app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            <v-avatar color="blue" size="48">
+              <span class="white--text headline">X</span>
+            </v-avatar> XX
+          </v-list-item-title>
+          <!-- <v-list-item-subtitle>subtext</v-list-item-subtitle> -->
+        </v-list-item-content>
+      </v-list-item>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <v-divider></v-divider>
 
-      <v-spacer></v-spacer>
+      <v-list dense>
+        <template v-for="item in items">
+          <v-row v-if="item.heading" :key="item.heading" align="center">
+            <v-col cols="6">
+              <v-subheader v-if="item.heading">{{ item.heading }}</v-subheader>
+            </v-col>
+            <v-col cols="6" class="text-center">
+              <a href="#!" class="body-2 black--text">EDIT</a>
+            </v-col>
+          </v-row>
+          <v-list-item v-else :key="item.text" @click="go(item.url)" link>
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.text }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+    <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app color="blue" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
+        <span class="hidden-sm-and-down">桂北课表</span>
+      </v-toolbar-title>
+      <v-spacer />
+      <v-btn icon>
+        <v-icon>mdi-bell</v-icon>
       </v-btn>
-    </v-app-bar>
+      <!-- <v-btn
+        icon
+        large
+      >
+        <v-avatar
+          size="32px"
+          item
+        >
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/logos/logo.svg"
+            alt="Vuetify"
+          /></v-avatar>
+      </v-btn>-->
+      <v-menu left bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
 
+        <v-list>
+          <v-list-item v-for="n in 5" :key="n" @click="() => {}">
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
     <v-content>
-      <router-view/>
+      <router-view />
     </v-content>
+    <v-footer>
+      <v-spacer></v-spacer>
+      <div>&copy; Kurokitu/Rubicon {{ new Date().getFullYear() }}</div>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-
+  props: {
+    source: String
+  },
   data: () => ({
-    //
+    dialog: false,
+    drawer: null,
+    items: [
+      { icon: "mdi-home", text: "主页", url: "/" },
+      { icon: "mdi-calendar-today", text: "课程表", url: "/Coures" },
+      { icon: "mdi-receipt", text: "你的课程", url: "/" },
+      { icon: "mdi-bookmark", text: "你选择的课程", url: "/" }
+    ]
   }),
+  methods: {
+    go(url) {
+      window.location.href = url;
+    }
+  }
 };
 </script>

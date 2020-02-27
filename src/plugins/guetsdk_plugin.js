8ff500f15c8ca460318bf6b2bc76f2346d3b02c9
thisLight <l1589002388@gmail.com>
2020-02-27 20:46:15 +0800

    update document for GUETPlugin

--- src/plugins/guetsdk_plugin.js
+++ src/plugins/guetsdk_plugin.js
@@ -18,7 +18,24 @@ class ValueHolder {
 
 export const ClientHolder = new ValueHolder(new GUETClient());
 
-
+/* This is a plugin for Vue.
+* It can inject a function $guet into Vue instances automatically.
+* ## How to
+* - All vue instance will contain $guet field
+* - If call this function with no argument, it will return current client.
+* - If call this function with one argument, it will set the argument to be the current client, then return it.
+* For example:
+* ````
+* methods: {
+*   login(){
+*     this.$guet().login(this.username,this.password).then((result) => ...);
+*   },
+*   switchUser(){
+*     queryUserById(this.selectedUserId).then((localUser) => this.$guet(localUser.asClient()));
+*   }
+* }
+* ````
+*/
 export class GUETPlugin {
     install(Vue){
         Vue.prototype.$guet = function(value) {

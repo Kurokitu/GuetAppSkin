3c114e148be2242cb2ae2e02adfcd334eeaffed7
thisLight <l1589002388@gmail.com>
2020-02-27 18:26:39 +0800

    add a value holder hold GUETClient

--- src/plugins/guetsdk_plugin.js
+++ src/plugins/guetsdk_plugin.js
@@ -1,8 +1,31 @@
 import { GUETClient } from "@/plugins/guetsdk";
 
 
+class ValueHolder {
+    constructor(initalValue){
+        this._value = initalValue;
+    }
+
+    get value(){
+        return this._value;
+    }
+
+    set value(v){
+        this._value = v;
+    }
+}
+
+
+export const ClientHolder = new ValueHolder(new GUETClient());
+
+
 export class GUETPlugin {
     install(Vue){
-        Vue.prototype.$guet = new GUETClient();
+        Vue.prototype.$guet = (value) => {
+            if (value !== undefined || value !== null){
+                ClientHolder.value = value;
+            }
+            return ClientHolder.value;
+        };
     }
 }

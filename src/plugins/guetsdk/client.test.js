7707f67d30d1aedb3ca70461851505979e4fec46
thisLight <l1589002388@gmail.com>
2020-02-29 23:04:30 +0800

    GUETSDK: fix tests for client

--- src/plugins/guetsdk/client.test.js
+++ src/plugins/guetsdk/client.test.js
@@ -1,14 +1,13 @@
 import { GUETClient } from './client';
 
 describe("GUETClient", () => {
-    it("can hook 'cookie_not_found' for case which cookie is not found in client", () => {
-        let calledTimes = 0;
-        let mock = () => { calledTimes++; };
+    it("can hook 'cookie_not_found' for case which cookie is not found in client", async () => {
+        let mock = jest.fn();
         let client = new GUETClient();
         client.on("cookie_not_found", mock);
-        let x = client.isLogin; // .isLogin will trigger event if cookie not found
+        let x = await client.askLogin(); // .isLogin will trigger event if cookie not found
         expect(x).toBe(false);
-        expect(calledTimes).toBe(1);
+        expect(mock).toBeCalledTimes(1);
     });
 
     it("can hook 'cookie_set' for listening cookie was setteing", () => {

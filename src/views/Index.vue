e9dcef0b73ad8e11eecaebd630c07c149ed839df
Kurokitu <foxiti.a@gmail.com>
2020-02-29 21:30:42 +0800

    Annotated carousel

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <with-app-bar>
-    <v-card class="mx-auto mb-6 pa-4" elevation="0" v-resize="onResize">
+    <!-- <v-card class="mx-auto mb-6 pa-4" elevation="0" v-resize="onResize">
       <v-carousel
         cycle
         :height="this.windowSize.x > 1024 ? 400 : 180"
@@ -9,7 +9,7 @@
       >
         <v-carousel-item v-for="(item,i) in items" :key="i" :src="item.src"></v-carousel-item>
       </v-carousel>
-    </v-card>
+    </v-card> -->
 
     <v-card class="pa-4 mx-auto" elevation="0">
       <strong class="display-1">你好,</strong>
